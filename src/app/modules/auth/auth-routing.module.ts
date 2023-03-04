7e6b284defe6ae638b79b2d6b785807fddacd9b8
Edycar Reyes <edycarreyes@gmail.com>
2023-03-04 17:58:52 -0600

    added the routes for the auth module in the auth-routing module file

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginViewComponent} from "./components/login-view/login-view.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginViewComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
