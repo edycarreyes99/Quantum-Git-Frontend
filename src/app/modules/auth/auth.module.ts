58e26ef7afa34691d664383447dbab27fbca223c
Edycar Reyes <edycarreyes@gmail.com>
2023-03-04 17:43:28 -0600

    Generated the files for the login-form-component

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginViewComponent } from './components/login-view/login-view.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
 
 
 @NgModule({
   declarations: [
-    LoginViewComponent
+    LoginViewComponent,
+    LoginFormComponent
   ],
   imports: [
     CommonModule,
