d0abcc2cba71068c8d91d0be1637f7d079099340
Edycar Reyes <edycarreyes@gmail.com>
2023-03-05 16:36:39 -0600

    added the root path url for the commits component view

--- src/app/modules/commits/commits-routing.module.ts
+++ src/app/modules/commits/commits-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {CommitsViewComponent} from "./components/commits-view/commits-view.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: CommitsViewComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class CommitsRoutingModule { }
+export class CommitsRoutingModule {
+}
