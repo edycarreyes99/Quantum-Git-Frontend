c2be9708d5e2462429b7e0112770172d466d2130
Edycar Reyes <edycarreyes@gmail.com>
2023-03-04 22:07:59 -0600

    changed the non-dynamic URLs in the index method of the repos.service file

--- src/app/modules/repos/services/repos.service.ts
+++ src/app/modules/repos/services/repos.service.ts
@@ -1,28 +1,31 @@
 import {Injectable} from '@angular/core';
 import {Octokit} from "octokit";
 import {GITHUB_ACCESS_TOKEN_LS} from "../../../core/constants/local-storage.constants";
+import {Repo} from "../interfaces/repo";
+import {REPO_INDEX_URL} from "../../../core/constants/api/repo.constants";
+import {DEFAULT_API_REQUEST_HEADERS} from "../../../core/constants/api/global-api.constants";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReposService {
   accessToken: string = '';
+  octokit: Octokit | undefined;
 
   constructor() {
     this.accessToken = localStorage.getItem(GITHUB_ACCESS_TOKEN_LS) ?? '';
-  }
-
-  async index() {
-    const octokit = new Octokit({
+    this.octokit = new Octokit({
       auth: this.accessToken
     });
+  }
 
-    await octokit.request('GET /user/repos', {
-      'X-GitHub-Api-Version': '2022-11-28'
-    }).then((repos) => {
-      console.log('Repositories are:', repos)
-    }).catch((error) => {
-      console.error('Error fetching repositories for the current user logged in:', error);
+  async index(): Promise<Repo[]> {
+    return new Promise<Repo[]>(async (resolve, rejects) => {
+      await this.octokit?.request(REPO_INDEX_URL, DEFAULT_API_REQUEST_HEADERS).then((repos) => {
+        console.log('Repositories are:', repos)
+      }).catch((error) => {
+        console.error('Error fetching repositories for the current user logged in:', error);
+      });
     });
   }
 }
