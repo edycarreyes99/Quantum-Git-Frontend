890fbe52786d68f0951db3dc572c13ac8be23787
Edycar Reyes <edycarreyes@gmail.com>
2023-03-04 21:49:14 -0600

    generated and styled the navbar component for the projec

---
+++ src/app/core/components/navbar/navbar.component.ts
@@ -0,0 +1,31 @@
+import {Component} from '@angular/core';
+import {AuthService} from "../../../modules/auth/services/auth.service";
+import {User} from '@angular/fire/auth';
+import {ActivatedRoute, Router} from "@angular/router";
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent {
+  user: User | undefined;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.user = this.authService.getCurrentUser();
+  }
+
+  // Method to do log out
+  async logout(): Promise<void> {
+    return new Promise<void>(async () => {
+      await this.authService.logout().then(async () => {
+        await this.router.navigate(['']);
+      }).catch((error) => {
+        console.error('Error logging out:', error);
+      });
+    });
+  }
+}
