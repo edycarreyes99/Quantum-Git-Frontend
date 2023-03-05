83b02ce88de4f5836ed2cfe8fd7b039eebcafa33
Edycar Reyes <edycarreyes@gmail.com>
2023-03-05 16:47:24 -0600

    configured the method to show notifications in the project

--- src/app/core/services/notifications/notifications.service.ts
+++ src/app/core/services/notifications/notifications.service.ts
@@ -1,9 +1,38 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {IndividualConfig, ToastrService} from "ngx-toastr";
+import {ERROR_TOAST, INFO_TOAST, SUCCESS_TOAST, WARNING_TOAST} from "../../constants/toast.constants";
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationsService {
 
-  constructor() { }
+  constructor(
+    private toast: ToastrService
+  ) {
+  }
+
+  // Method to show a toast
+  showToast(type: string, title: string, message: string): void {
+    const toastSettings: Partial<IndividualConfig> = {
+      timeOut: 7000,
+      closeButton: true,
+      progressBar: true,
+      progressAnimation: 'increasing',
+    };
+    if (type === SUCCESS_TOAST) {
+      this.toast.success(`${message}`, `${title}`, toastSettings);
+    } else if (type === ERROR_TOAST) {
+      this.toast.error(`${message}`, `${title}`, toastSettings);
+    } else if (type === WARNING_TOAST) {
+      this.toast.warning(`${message}`, `${title}`, toastSettings);
+    } else if (type === INFO_TOAST) {
+      this.toast.info(`${message}`, `${title}`, toastSettings);
+    }
+  }
+
+  // Method to delete all the toast showed in the view
+  clearAllToast(): void {
+    this.toast.clear();
+  }
 }
