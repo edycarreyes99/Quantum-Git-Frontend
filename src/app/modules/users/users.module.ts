ef8ea52e13795156fc8681a7ae54c46f5ffeb022
Edycar Reyes <edycarreyes@gmail.com>
2023-03-05 13:51:55 -0600

    generated the files for the uer info component

--- src/app/modules/users/users.module.ts
+++ src/app/modules/users/users.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { UsersRoutingModule } from './users-routing.module';
 import { UsersViewComponent } from './components/users-view/users-view.component';
+import { UserInfoComponent } from './components/user-info/user-info.component';
 
 
 @NgModule({
   declarations: [
-    UsersViewComponent
+    UsersViewComponent,
+    UserInfoComponent
   ],
   imports: [
     CommonModule,
