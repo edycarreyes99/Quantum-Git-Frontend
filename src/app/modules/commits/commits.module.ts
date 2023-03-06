fb6193be1b51071b5d0245e4a4706c245eb77ba4
Edycar Reyes <edycarreyes@gmail.com>
2023-03-05 18:48:14 -0600

    generated the files for the commits list component

--- src/app/modules/commits/commits.module.ts
+++ src/app/modules/commits/commits.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { CommitsRoutingModule } from './commits-routing.module';
 import { CommitsViewComponent } from './components/commits-view/commits-view.component';
+import { CommitsListComponent } from './components/commits-list/commits-list.component';
 
 
 @NgModule({
   declarations: [
-    CommitsViewComponent
+    CommitsViewComponent,
+    CommitsListComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/modules/commits/components/commits-list/commits-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-commits-list',
+  templateUrl: './commits-list.component.html',
+  styleUrls: ['./commits-list.component.scss']
+})
+export class CommitsListComponent {
+
+}
