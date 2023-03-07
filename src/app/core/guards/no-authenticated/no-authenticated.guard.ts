76a0e52d81ca226d7378d399da51165affc51e79
Edycar Reyes <edycarreyes@gmail.com>
2023-03-06 23:00:10 -0600

    generated the logic for grant access to the no-authenticated users to the routes

--- src/app/core/guards/no-authenticated/no-authenticated.guard.ts
+++ src/app/core/guards/no-authenticated/no-authenticated.guard.ts
@@ -1,15 +1,28 @@
-import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
+import {Observable} from 'rxjs';
+import {AuthService} from "../../../modules/auth/services/auth.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoAuthenticatedGuard implements CanActivate {
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {
+  }
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    if (this.authService.getCurrentUserFromFirebase()) {
+      this.router.navigate(['/auth/login']);
+      return false;
+    } else {
+      return true;
+    }
   }
-  
+
 }
