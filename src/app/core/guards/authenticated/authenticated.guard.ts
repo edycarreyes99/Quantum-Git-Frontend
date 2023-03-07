7c1ed66f81399faae33b2807988b3c223007f861
Edycar Reyes <edycarreyes@gmail.com>
2023-03-06 22:59:00 -0600

    generated the logic for grant access to the authenticated users to the routes

--- src/app/core/guards/authenticated/authenticated.guard.ts
+++ src/app/core/guards/authenticated/authenticated.guard.ts
@@ -1,15 +1,27 @@
-import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
+import {Observable} from 'rxjs';
+import {AuthService} from "../../../modules/auth/services/auth.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticatedGuard implements CanActivate {
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {
+  }
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    if (this.authService.getCurrentUserFromFirebase()) {
+      return true;
+    } else {
+      this.router.navigate(['/auth']);
+      return false;
+    }
   }
-  
+
 }
