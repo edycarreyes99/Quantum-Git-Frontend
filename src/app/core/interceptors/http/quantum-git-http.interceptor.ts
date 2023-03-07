a817ab052019b5172c4eebdf868d3e3fe4fecda8
Edycar Reyes <edycarreyes@gmail.com>
2023-03-06 22:40:10 -0600

    created the logic to intercept the http requests and inject the current user authenticated JWT token in the headers

--- src/app/core/interceptors/http/quantum-git-http.interceptor.ts
+++ src/app/core/interceptors/http/quantum-git-http.interceptor.ts
@@ -1,18 +1,42 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
   HttpInterceptor
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {from, Observable, switchMap} from 'rxjs';
+import {AuthService} from "../../../modules/auth/services/auth.service";
 
 @Injectable()
 export class QuantumGitHttpInterceptor implements HttpInterceptor {
+  excluded: string[] = ['auth', 'assets'];
 
-  constructor() {}
+  constructor(
+    private authService: AuthService,
+  ) {
+  }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
+    for (const excludedUrl of this.excluded) {
+      if (request.url.includes(excludedUrl)) {
+        return next.handle(request);
+      }
+    }
+
+    return from(this.authService.getToken()).pipe(
+      switchMap((token: string) => {
+        const req = request.clone({
+          url: request.url,
+          setParams: {
+            token,
+          },
+          setHeaders: {
+            Authorization: 'Bearer ' + token
+          }
+        });
+        return next.handle(req);
+      })
+    );
   }
 }
