cd5cdafab0522633e077c1fa292d02743379712e
Edycar Reyes <edycarreyes@gmail.com>
2023-03-08 01:09:40 -0600

    implemented the CRUD abstract class to the users service

--- src/app/modules/users/services/users.service.ts
+++ src/app/modules/users/services/users.service.ts
@@ -1,9 +1,16 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {CRUD} from "../../../core/http/crud";
+import {IUser} from "../interfaces/user";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
-export class UsersService {
+export class UsersService extends CRUD<IUser, IUser[]> {
 
-  constructor() { }
+  protected constructor(
+    protected override http: HttpClient
+  ) {
+    super(http, '');
+  }
 }
