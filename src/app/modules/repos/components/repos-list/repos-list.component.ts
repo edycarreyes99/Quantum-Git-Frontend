1b75ad22825ab135d32662801635ed6a14b2aba9
Edycar Reyes <edycarreyes@gmail.com>
2023-03-08 00:59:29 -0600

    implemented the new CRUD abstract class in the repos service and repos-list component

--- src/app/modules/repos/components/repos-list/repos-list.component.ts
+++ src/app/modules/repos/components/repos-list/repos-list.component.ts
@@ -1,12 +1,12 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {ReposService} from "../../services/repos.service";
 import {IRepo} from "../../interfaces/repo";
-import {RepoPaginationOptions} from "../../models/repo-pagination-options";
 import {
   QuantumGitPaginatorComponent
 } from "../../../../core/components/quantum-git-paginator/quantum-git-paginator.component";
 import {NotificationsService} from "../../../../core/services/notifications/notifications.service";
 import {ERROR_TOAST, WARNING_TOAST} from "../../../../core/constants/toast.constants";
+import {IPagination} from "../../../../core/interfaces/pagination";
 
 @Component({
   selector: 'app-repos-list',
@@ -14,13 +14,11 @@ import {ERROR_TOAST, WARNING_TOAST} from "../../../../core/constants/toast.const
   styleUrls: ['./repos-list.component.scss']
 })
 export class ReposListComponent implements OnInit {
-  // ViewChild Variables
-  @ViewChild('quantumGitPaginator') quantumGitPaginator: QuantumGitPaginatorComponent | undefined;
-
   // Component Variables
   repos: IRepo[] = [];
   loading: boolean = true;
   empty: boolean = false;
+  pagination: IPagination | undefined;
 
   constructor(
     private reposService: ReposService,
@@ -35,50 +33,61 @@ export class ReposListComponent implements OnInit {
   async fetchAllRepos(page?: number | undefined): Promise<IRepo[]> {
     return new Promise<IRepo[]>(async (resolve, rejects) => {
       this.loading = true;
-      const paginationOptions = new RepoPaginationOptions();
 
-      if (!!page)
-        paginationOptions.page = page;
+      await this.reposService.index({
+        page: page ?? 1,
+        per_page: 35
+      }).subscribe({
+        next: (reposResponse) => {
+          this.repos = reposResponse.data;
+          this.loading = false;
 
-      await this.reposService.index(paginationOptions).then((repos) => {
-        this.repos = repos;
-        this.loading = false;
-        if (this.repos.length === 0)
-          this.empty = true;
-        setTimeout(() => {
-          if (!this.empty)
-            this.quantumGitPaginator?.parsePaginationString(this.repos[0].pagination)
-        }, 500);
-        window.scroll({
-          top: 0,
-          left: 0,
-          behavior: 'smooth'
-        });
-        if (this.empty) {
-          this.notificationService
-            .showToast(
-              WARNING_TOAST,
-              'Content is empty',
-              'The repositories list is empty.'
-            );
+          const {first, next, previous, last} = {...reposResponse};
+
+          this.pagination = {
+            first,
+            next,
+            previous,
+            last
+          };
+
+          if (this.repos.length === 0)
+            this.empty = true;
+
+          window.scroll({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+          });
+
+          if (this.empty) {
+            this.notificationService
+              .showToast(
+                WARNING_TOAST,
+                'Content is empty',
+                'The repositories list is empty.'
+              );
+          }
+
+          resolve(reposResponse.data);
+        },
+        error: (error) => {
+          console.error('Error fetching repos:', error);
+          this.loading = false;
+          if (this.repos.length === 0)
+            this.empty = true;
+          window.scroll({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+          });
+          this.notificationService.showToast(
+            ERROR_TOAST,
+            'Error Fetching Repos',
+            'An error occurred while fetching the repos list.'
+          );
+          rejects(error);
         }
-        resolve(repos);
-      }).catch((error) => {
-        console.error('Error fetching repos:', error);
-        this.loading = false;
-        if (this.repos.length === 0)
-          this.empty = true;
-        window.scroll({
-          top: 0,
-          left: 0,
-          behavior: 'smooth'
-        });
-        this.notificationService.showToast(
-          ERROR_TOAST,
-          'Error Fetching Repos',
-          'An error occurred while fetching the repos list.'
-        );
-        rejects(error);
       });
     });
   }
--- src/app/modules/repos/services/repos.service.ts
+++ src/app/modules/repos/services/repos.service.ts
@@ -1,36 +1,16 @@
 import {Injectable} from '@angular/core';
-import {Octokit} from "octokit";
-import {GITHUB_ACCESS_TOKEN_LS} from "../../../core/constants/local-storage.constants";
 import {IRepo} from "../interfaces/repo";
 import {REPOS_URL} from "../../../core/constants/api/repo.constants";
-import {IRepoPaginationOptions} from "../interfaces/repo-pagination-options";
+import {CRUD} from "../../../core/http/crud";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
-export class ReposService {
-  // Service Variables
-  accessToken: string = '';
-  octokit: Octokit | undefined;
-
-  constructor() {
-    this.accessToken = localStorage.getItem(GITHUB_ACCESS_TOKEN_LS) ?? '';
-    this.octokit = new Octokit({
-      auth: this.accessToken
-    });
-  }
-
-  async index(paginationOptions: IRepoPaginationOptions): Promise<IRepo[]> {
-    return new Promise<IRepo[]>(async (resolve, rejects) => {
-      await this.octokit?.request(REPOS_URL, paginationOptions as any).then((repos: Record<string, any>) => {
-        repos['data'].forEach((repo: IRepo) => {
-          repo.pagination = repos['headers']['link'];
-        })
-        resolve(repos['data']);
-      }).catch((error) => {
-        console.error('Error fetching repositories for the current user logged in:', error);
-        rejects(error);
-      });
-    });
+export class ReposService extends CRUD<IRepo> {
+  protected constructor(
+    protected override http: HttpClient,
+  ) {
+    super(http, REPOS_URL);
   }
 }
